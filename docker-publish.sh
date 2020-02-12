c15a785fc87dd6843935145a7892e94bdc58d4ce
Joao Duarte <jsvduarte@gmail.com>
2020-02-12 15:31:31 +0000

    don't use env var for gem push

--- docker-publish.sh
+++ docker-publish.sh
@@ -2,6 +2,4 @@
 
 # This is intended to be run inside the docker container as the command of the docker-compose.
 set -ex
-docker-compose -f .ci/docker-compose.yml run logstash bundle exec rake vendor
-docker-compose -f .ci/docker-compose.yml run -e GEM_HOST_API_KEY logstash echo $GEM_HOST_API_KEY
-docker-compose -f .ci/docker-compose.yml run -e GEM_HOST_API_KEY logstash bundle exec rake publish_gem
+docker-compose -f .ci/docker-compose.yml run -e GEM_HOST_API_KEY logstash .ci/publish.sh
--- publish.sh
+++ publish.sh
@@ -1,2 +1,4 @@
 bundle exec rake vendor
+echo "---\n:rubygems_api_key: ${GEM_HOST_API_KEY}" > ~/.gem/credentials
+chmod 0600 ~/.gem/credentials
 bundle exec rake publish_gem
