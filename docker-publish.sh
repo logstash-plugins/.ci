ced650cb03a8978f93ccbf6eb161ec78497d0357
Joao Duarte <jsvduarte@gmail.com>
2020-02-12 21:58:48 +0000

    also pass in the github token

--- docker-publish.sh
+++ docker-publish.sh
@@ -2,4 +2,4 @@
 
 # This is intended to be run inside the docker container as the command of the docker-compose.
 set -ex
-docker-compose -f .ci/docker-compose.yml run -e GEM_HOST_API_KEY logstash .ci/publish.sh
+docker-compose -f .ci/docker-compose.yml run -e GEM_HOST_API_KEY -e GITHUB_TOKEN logstash .ci/publish.sh
