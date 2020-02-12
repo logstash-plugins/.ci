5cd7f89a3842d0f9287b82fb0c3ce71aa117263e
Joao Duarte <jsvduarte@gmail.com>
2020-02-12 12:48:17 +0000

    pass in rubygems key

--- docker-publish.sh
+++ docker-publish.sh
@@ -3,4 +3,4 @@
 # This is intended to be run inside the docker container as the command of the docker-compose.
 set -ex
 docker-compose -f .ci/docker-compose.yml run logstash bundle exec rake vendor
-docker-compose -f .ci/docker-compose.yml run logstash bundle exec rake publish_gem
+docker-compose -f .ci/docker-compose.yml run logstash -e GEM_HOST_API_KEY bundle exec rake publish_gem
