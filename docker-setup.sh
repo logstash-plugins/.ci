fad16987df5bc0a0768c2f4232eb06d25234427e
João Duarte <jsvd@users.noreply.github.com>
2025-08-04 10:24:56 +0100

    Update matrix.yml to account for newer 9x and the last 8.x minor (#76)

--- docker-setup.sh
+++ docker-setup.sh
@@ -13,7 +13,7 @@ check_docker_snapshot() {
     echo "docker image exists. proceeding..."
   else
     case $stack_version_alias in
-      "8.previous"|"8.current"|"8.next"|"8.future")
+      "8.previous"|"8.current"|"9.previous"|"9.current"|"9.next")
         exit 99
         ;;
       *)
@@ -27,7 +27,7 @@ VERSION_URL="https://raw.githubusercontent.com/elastic/logstash/main/ci/logstash
 
 if [ -z "${ELASTIC_STACK_VERSION}" ]; then
     echo "Please set the ELASTIC_STACK_VERSION environment variable"
-    echo "For example: export ELASTIC_STACK_VERSION=7.x"
+    echo "For example: export ELASTIC_STACK_VERSION=9.x"
     exit 1
 fi
 
@@ -50,8 +50,8 @@ if [[ "$ELASTIC_STACK_RETRIEVED_VERSION" != "null" ]]; then
   ELASTIC_STACK_RETRIEVED_VERSION="${ELASTIC_STACK_RETRIEVED_VERSION#\"}"
   echo "Translated $ELASTIC_STACK_VERSION to ${ELASTIC_STACK_RETRIEVED_VERSION}"
   export ELASTIC_STACK_VERSION=$ELASTIC_STACK_RETRIEVED_VERSION
-elif [[ "$ELASTIC_STACK_VERSION" == "8.next" ]]; then
-  # we know "8.next" only exists between FF and GA of a minor
+elif [[ "$ELASTIC_STACK_VERSION" == "9.next" ]]; then
+  # we know "9.next" only exists between FF and GA of a minor
   # exit 99 so the build is skipped
   exit 99
 fi
